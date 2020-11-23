bf9c91ab6fc47260eaa67eab72bc505cca10c12b
Christie Metson <christie@sharesies.co.nz>
2020-11-23 17:01:49 +1300

    Add react router to our project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createContext } from "react"; // just create context sub-module
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export const MyContext = createContext({}); // just this piece of code
 
@@ -11,11 +12,29 @@ const myObject = {
   hi: "hello",
 };
 
+const About = () => <div>About page!</div>;
+
+const Users = () => <div>Users page!</div>;
+
+const Home = () => <div>Home page</div>;
+
 ReactDOM.render(
   <React.StrictMode>
-    <MyContext.Provider value={myObject}>
-      <App />
-    </MyContext.Provider>
+    <Router>
+      <Switch>
+        <MyContext.Provider value={myObject}>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <App />
+          </Route>
+        </MyContext.Provider>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
